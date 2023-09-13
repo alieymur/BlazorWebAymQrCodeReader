7b1341a8fb25835d9cdeb709708656087b498618
Ali Eymur <eymur@hipermedya.com>
2023-09-13 20:23:17 +0300

    a

---
+++ wwwroot/js/camhelper-v1.js
@@ -0,0 +1,31 @@
+﻿const constraints = { audio: false, video: true };
+var video;
+
+export async function initialize(v, dotnet) {
+   video = v;
+   let stream = await navigator.mediaDevices.getUserMedia(constraints);
+   video.srcObject = stream;
+   video.play();
+}
+export function getBase64Img(video) {
+    let canvas = document.createElement("canvas");
+    let context = canvas.getContext('2d');
+    canvas.setAttribute('width', video.videoWidth);
+    canvas.setAttribute('height', video.videoHeight);
+    context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+    let data = canvas.toDataURL('image/png');
+    canvas.remove();
+    return data;
+}
+
+function errorMsg(msg, error, dotnet) {
+    dotnet.invokeMethodAsync("OnWebCamLiveError", msg);
+}
+
+const updateCanvas = (now, metadata) => {
+    DotNet.invokeMethodAsync('WebCamLabWASM','OnFrameArrived', getBase64Img(video));
+    video.requestVideoFrameCallback(updateCanvas);
+};
+
+video.requestVideoFrameCallback(updateCanvas);
+
