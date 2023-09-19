221e47d93564ade23fd279c66fa74cbcaee90867
Ali Eymur <eymur@hipermedya.com>
2023-09-19 18:25:12 +0300

    update 3

--- wwwroot/js/qrcodehelper.js
+++ wwwroot/js/qrcodehelper.js
@@ -7,18 +7,7 @@ export async function init(videoElementRef, dotnetObjectRef) {
             {
                 audio:false,
                 //video: true
-                video: {
-                    width: {
-                        min: 1280,
-                        ideal: 1920,
-                        max: 2560,
-                    },
-                    height: {
-                        min: 720,
-                        ideal: 1080,
-                        max: 1440
-                    },
-                    facingMode: 'environment'
+                facingMode: 'environment'
                 }
             });
         onSuccess(stream, videoElementRef);
