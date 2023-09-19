e1498fe29f5a836022f4bd5afc5aed2876759108
Ali Eymur <eymur@hipermedya.com>
2023-09-19 13:48:43 +0300

    camera

--- wwwroot/js/camhelper.js
+++ wwwroot/js/camhelper.js
@@ -1,10 +1,33 @@
 ﻿export async function init(videoElementRef, dotnetObjectRef) {
     //console.log("Init");
+    var getMedia = navigator.mediaDevices.getUserMedia
+        || navigator.getUserMedia
+        || navigator.webkitGetUserMedia
+        || navigator.mozGetUserMedia
+        || navigator.msGetUserMedia;
+    console.log("Exception occurred", getMedia);
+    if (!getMedia) {
+        onFailure(getMedia, dotnetObjectRef)
+    }
+
     try {
         var stream = await navigator.mediaDevices.getUserMedia(
             {
+                audio:false,
                 //video: true
-                facingMode: 'environment'
+                video: {
+                    width: {
+                        min: 1280,
+                        ideal: 1920,
+                        max: 2560,
+                    },
+                    height: {
+                        min: 720,
+                        ideal: 1080,
+                        max: 1440
+                    },
+                    facingMode: 'environment'
+                }
             });
         onSuccess(stream, videoElementRef);
         dotnetObjectRef.invokeMethodAsync("OnSuccess");
@@ -20,6 +43,6 @@ function onSuccess(stream, videoElementRef) {
 }
 
 function onFailure(exception, dotnetObjectRef) {
-    console.log("Exception occurred", exception);
+  //  console.log("Exception occurred", exception);
     dotnetObjectRef.invokeMethodAsync("onFailure", exception.message);
 }
