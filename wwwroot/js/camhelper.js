960737e8c915929de81340f1842e5dec6dc350c6
Ali Eymur <eymur@hipermedya.com>
2023-09-22 14:32:38 +0300

    9

--- wwwroot/js/camhelper.js
+++ wwwroot/js/camhelper.js
@@ -1,25 +1,12 @@
 ﻿export async function init(videoElementRef, dotnetObjectRef) {
     //console.log("Init");
-    //var getMedia = navigator.mediaDevices.getUserMedia
-    //    || navigator.getUserMedia
-    //    || navigator.webkitGetUserMedia
-    //    || navigator.mozGetUserMedia
-    //    || navigator.msGetUserMedia;
-    //if (!getMedia) {
-    //    console.log("Exception occurred", getMedia);
-    //    onFailure("Bu cihaz kamera erişimin desteklemiyor", dotnetObjectRef);
-    //    return;
-    //}
+    var contains = {
+        audio: false,
+        video: { facingMode: 'environment'}
+    }
 
     try {
-        var stream = await navigator.mediaDevices.getUserMedia(
-            {
-                audio:false,
-                //video: true
-                video: {
-                    facingMode: 'environment'
-                }
-            });
+        var stream = await navigator.mediaDevices.getUserMedia(contains);
         onSuccess(stream, videoElementRef);
         dotnetObjectRef.invokeMethodAsync("OnSuccess");
     }
@@ -34,24 +21,17 @@ function onSuccess(stream, videoElementRef) {
 }
 
 function onFailure(exception, dotnetObjectRef) {
-  //  console.log("Exception occurred", exception);
+    //  console.log("Exception occurred", exception);
     dotnetObjectRef.invokeMethodAsync("onFailure", exception.message);
 }
 
 export function getBase64Img(video, canvas) {
 
-    //let canvas = document.createElement("canvas");
-    //let canvas = document.getElementById("currentFrame");
-    //let canvas = CanvasRef;
     let context = canvas.getContext('2d');
     canvas.setAttribute('width', video.videoWidth);
     canvas.setAttribute('height', video.videoHeight);
     context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
     let data = canvas.toDataURL('image/png');
-
-    //canvas.removeAttribute('width');
-    //canvas.removeAttribute('height');
-    //canvas.remove();
     return data;
 }
 
